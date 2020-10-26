2137a05a7731ec8a2ad36e0d25d29872f8f024b1
czf1024 <1009823094@qq.com>
2020-10-26 16:12:04 +0800

    代码框架更新

---
+++ dao/src/main/java/com/czf/apply/dao/entity/GameItem.java
@@ -0,0 +1,17 @@
+package com.czf.apply.dao.entity;
+
+import lombok.Data;
+
+/**
+ * @author 10098
+ */
+@Data
+public class GameItem {
+
+    //意向部门id
+    private String id;
+    //意向部门
+    private String department;
+    //所在班级
+    private String className;
+}
